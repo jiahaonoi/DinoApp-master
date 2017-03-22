a002224841e5a9d298db2fa05bc75bc616714440
gsig123 <sigurbergsson94@gmail.com>
2017-03-22 12:21:48 +0000

    Added SearchParams object. That object created the need for SortBy.java, Type.java and PriceRange.java in order for it to match our JSON object exactly.

---
+++ app/src/main/java/com/example/ornol/dinoapp/searchParams/SearchParams.java
@@ -0,0 +1,56 @@
+package com.example.ornol.dinoapp.searchParams;
+
+/**
+ * Created by gudbjartursigurbergsson on 22/03/2017.
+ */
+
+public class SearchParams {
+    private String searchBar;
+    private Type[] types;
+    private PriceRange priceRange;
+    // Some sortBy in "sortBy[]" needs to be checked.
+    private SortBy[] sortBy;
+    private SortBy[] ordering;
+
+    public SearchParams() {}
+
+    public String getSearchBar() {
+        return searchBar;
+    }
+
+    public void setSearchBar(String searchBar) {
+        this.searchBar = searchBar;
+    }
+
+    public Type[] getTypes() {
+        return types;
+    }
+
+    public void setTypes(Type[] types) {
+        this.types = types;
+    }
+
+    public PriceRange getPriceRange() {
+        return priceRange;
+    }
+
+    public void setPriceRange(PriceRange priceRange) {
+        this.priceRange = priceRange;
+    }
+
+    public SortBy[] getSortBy() {
+        return sortBy;
+    }
+
+    public void setSortBy(SortBy[] sortBy) {
+        this.sortBy = sortBy;
+    }
+
+    public SortBy[] getOrdering() {
+        return ordering;
+    }
+
+    public void setOrdering(SortBy[] ordering) {
+        this.ordering = ordering;
+    }
+}
