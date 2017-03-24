f59c12f9836e0bd1e9ab17bc10c561a0f1729bac
jiahaonoi <jiahaonoi@Hotmail.com>
2017-03-24 14:20:44 +0000

    Merge branch 'master' of https://github.com/jiahaonoi/DinoApp-master
    # Conflicts:
    #	.idea/vcs.xml
    #	app/src/main/res/layout/fragment_offerlist_list.xml

--- app/src/main/java/com/example/ornol/dinoapp/MainActivity.java
+++ app/src/main/java/com/example/ornol/dinoapp/MainActivity.java
@@ -1,44 +1,177 @@
 package com.example.ornol.dinoapp;
 
-import android.app.FragmentManager;
-import android.content.Intent;
+import android.media.Image;
 import android.net.Uri;
-import android.support.v4.app.FragmentActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.widget.TextViewCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
+import android.view.MenuItem;
+import 	android.view.MenuInflater;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.Toast;
 
-public class MainActivity extends FragmentActivity implements BlankFragment.OnFragmentInteractionListener,Signup_Fragment.OnFragmentInteractionListener{
+import java.util.ArrayList;
 
-    private static BlankFragment editNameDialogFragment = new BlankFragment();
-    private static Signup_Fragment SDialogFragment = new Signup_Fragment();
+
+import com.example.ornol.dinoapp.http.ApiCall;
+import com.example.ornol.dinoapp.json.JsonJavaConverter;
+import com.example.ornol.dinoapp.searchParams.PriceRange;
+import com.example.ornol.dinoapp.searchParams.SearchParams;
+import com.example.ornol.dinoapp.searchParams.SortBy;
+import com.example.ornol.dinoapp.searchParams.Type;
+//import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Objects;
+
+//import okhttp3.OkHttpClient;
+
+
+public class MainActivity extends AppCompatActivity implements LoginFragment.OnFragmentInteractionListener,SignupFragment.OnFragmentInteractionListener{
+
+    // OkHttpClient for API Calls
+   // private OkHttpClient client;
+
+    private static LoginFragment editNameDialogFragment = new LoginFragment();
+    private static SignupFragment SDialogFragment = new SignupFragment();
+    private List<Object> myList = new ArrayList<Object>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        populateMyList();
+        populateListView();
+        ClickCallback();
+        // Initialize the OkHttpClient
+        //client = new OkHttpClient();
     }
     public void onFragmentInteraction(Uri uri){
 
-    }
-    public void DisplayLogin(View view){
-        //Intent intent = new Intent (this, Activity_sigh_up.class);
-        //startActivity(intent);
-        showLoginDialog();
     }
 
     private void showLoginDialog() {
         android.support.v4.app.FragmentManager fm = getSupportFragmentManager();
         editNameDialogFragment.show(fm, "fragment_edit_name");
     }
+
     private void showSignupDialog() {
         android.support.v4.app.FragmentManager fm = getSupportFragmentManager();
         SDialogFragment.show(fm, "fragment_edit_name");
     }
-    public void DisplaySignUp(View view){
-        showSignupDialog();
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
     }
-    public void DisplayMenus(View view){
-        //Intent intent = new Intent (this, Activity_sigh_up.class);
-        //startActivity(intent);
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        switch (item.getItemId()) {
+            case R.id.login:
+                showLoginDialog();
+                break;
+            case R.id.signup:
+                showSignupDialog();
+                break;
+
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
+
+    //-----------------------------------------ListView-----------------------------
+    //set data on myList.
+    public void populateMyList(){
+        myList.add(new Object[]{"Humberger Menu1",1400,"With vegetable, fries and coke"});
+        myList.add(new Object[]{"Humberger Menu2",1600,"With bacon, vegetable, fries and coke"});
+        myList.add(new Object[]{"Humberger Menu3",1800,"With bacon, egg, vegetable, fries and coke"});
+        myList.add(new Object[]{"Humberger Menu4",2000,"With extra humberger, bacon, egg, vegetable, fries and coke"});
+    }
+
+    public void populateListView(){
+        ArrayAdapter<Object> adapter = new MyListAdapter();
+        ListView list = (ListView) findViewById(R.id.list);
+        list.setAdapter(adapter);
+    }
+
+    //Adapter for ListView
+    private class MyListAdapter extends ArrayAdapter<Object>{
+        public MyListAdapter(){
+            super(MainActivity.this,R.layout.activity_main, myList);
+        }
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View itemView = convertView;
+            if (itemView == null) {
+                itemView = getLayoutInflater().inflate(R.layout.item_view, parent, false);
+            }
+            Object[] currentItem = (Object[]) myList.get(position);
+            ImageView imageView = (ImageView) itemView.findViewById(R.id.image);
+            imageView.setImageResource(R.drawable.humberger);
+
+            TextView name = (TextView) itemView.findViewById(R.id.name);
+            name.setText("Name:" + currentItem[0]);
+
+            TextView price = (TextView) itemView.findViewById(R.id.price);
+            price.setText("Price:" + (int) currentItem[1]);
+
+            TextView description = (TextView) itemView.findViewById(R.id.description);
+            description.setText("Description:" + currentItem[2]);
+            return itemView;
+        }
+    }
+    //set onItemClick for ListView.
+    private void ClickCallback(){
+        final ListView list = (ListView)findViewById(R.id.list);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent,View viewClicked, int position, long id){
+                int itemPosition     = position;
+
+                // Show Alert
+                Toast.makeText(getApplicationContext(),
+                        "Position :"+itemPosition, Toast.LENGTH_LONG)
+                        .show();
+            }
+        });
+    }
+    //-----------------------------------------ListView-----------------------------
+
+    // Loads Offers From Web Server based on jsonString representation of searchParams.
+   /* private void loadOffers(final String jsonString, final String url) {
+        Log.d("Here We ", "Are");
+        new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... params){
+                try {
+                    String response = ApiCall.POST(client, url, jsonString);
+                    Log.d("Response", response);
+                    JsonJavaConverter jsonJavaConverter = new JsonJavaConverter();
+                    List<Offer> offerList = jsonJavaConverter.jsonStringToListOfJavaObjects(response);
+                    Log.d("Offer List ", String.valueOf(offerList.get(0)));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+        }.execute();
+    }*/
 }
